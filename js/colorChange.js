a77e9d1ab8966f53754ee4e7c25c7eaa20972c86
Aidan Scott <160932113+AidanJScott@users.noreply.github.com>
2024-12-03 10:19:00 -0500

    Added print style sheet

---
+++ js/colorChange.js
@@ -0,0 +1,10 @@
+// Aidan Scott
+"use strict";
+
+// select background icon
+let backgroundIcon = document.querySelector("main img");
+
+// add color palette change on double click
+backgroundIcon.addEventListener("click", event =>{
+    console.log("hi");
+});
